419d9dddee1e2d381361c728428ee1088b5ac6d9
davbyr <dbyrne@noc.ac.uk>
2021-06-14 11:16:53 +0100

    Added flow calculation to tvalidate_transects

--- validate_transects.py
+++ validate_transects.py
@@ -19,17 +19,32 @@ def write_ds_to_file(ds, fn, **kwargs):
     with ProgressBar():
         ds.to_netcdf(fn, **kwargs)
 
-def extract_transects_t(fn_nemo_data, fn_nemo_domain,
+def extract_transects_t(fn_nemo_data_t, fn_nemo_domain,
                       A_lon, A_lat, B_lon, B_lat,
-                      dn_out, fn_out_pre = 'transect', transect_names=[]):
+                      dn_out, fn_nemo_data_u=None, fn_nemo_data_v=None,
+                      fn_out_pre = 'transect', transect_names=[],
+                      nemo_chunks = {'time_counter':250}, multiple=True):
     
-    nemo = coast.NEMO(fn_nemo_data, fn_nemo_domain, 
-                      grid_ref='t-grid', chunks={'time_counter':100}, multiple=True)
+    nemo = coast.NEMO(fn_nemo_data_t, fn_nemo_domain, 
+                      grid_ref='t-grid', chunks=nemo_chunks, multiple=multiple)
+    
+    if fn_nemo_data_u is not None:
+        nemo_u = coast.NEMO(fn_nemo_data_u, fn_nemo_domain, 
+                      grid_ref='u-grid', chunks=nemo_chunks, multiple=multiple)
+        nemo_v = coast.NEMO(fn_nemo_data_v, fn_nemo_domain, 
+                      grid_ref='v-grid', chunks=nemo_chunks, multiple=multiple)
+        nemo_f = coast.NEMO(fn_domain = fn_nemo_domain, grid_ref='f-grid', chunks={})
+    
     n_transect = len(A_lon)
     
     for ii in range(n_transect):
         print('Transect {0}/{1}'.format(ii, n_transect))
         tran = coast.Transect_t( nemo, (A_lon[ii], A_lat[ii]), (B_lon[ii], B_lat[ii]))
+        
+        if fn_nemo_data_u is not None:
+            tran_f = coast.Transect_f(nemo_f, (A_lon[ii], A_lat[ii]), (B_lon[ii], B_lat[ii])) 
+            tran_f.calc_flow_across_transect(nemo_u,nemo_v)
+            tran = xr.merge((tran, tran_f))
         
         ds = tran.data
         ds['x_ind'] = (['r_dim'], tran.x_ind)
@@ -46,11 +61,7 @@ def extract_transects_t(fn_nemo_data, fn_nemo_domain,
         fn_out = os.path.join(dn_out, fn_out)
         write_ds_to_file(ds, fn_out)
 
-def validate_transects_t():
-    return
 
-def validate_transects_flow():
-    return
     
 
 
