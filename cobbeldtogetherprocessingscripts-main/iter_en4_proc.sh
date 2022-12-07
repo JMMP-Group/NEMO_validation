4380e515cd6abea764482924c66097ef240782e0
jpolton <jpolton@gmail.com>
2022-12-07 20:38:53 +0000

    abstract machine name from preprocessing sh call

--- cobbeldtogetherprocessingscripts-main/iter_en4_proc.sh
+++ cobbeldtogetherprocessingscripts-main/iter_en4_proc.sh
@@ -7,14 +7,14 @@ for (( start=$STARTYEAR; start<$(expr $ENDYEAR + 1); start++ ))
 do
   for (( month=1; month<13; month++ ))
   do
-     end=$(expr $start + 1 )
-#     sbatch spice_ana_SEAS_P0.0.sh $SEAS $start $end
-     if [ $MACHINE = "SPICE" ]; then
-      echo "sbatch spice_pre_process_en4_monthly.sh $start $month"
-      sbatch spice_pre_process_en4_monthly.sh $start $month  # THIS FILE IS MISSING
-     elif [ $MACHINE = "LOTUS" ]; then
-      echo "sbatch lotus_pre_process_en4_monthly.sh $start $month"
-      sbatch lotus_pre_process_en4_monthly.sh $start $month
+    if [ $MACHINE = "SPICE" ]; then
+      echo "sbatch "+${MACHINE,,}+"_pre_process_en4_monthly.sh $start $month"
+      sbatch spice_pre_process_en4_monthly.sh $start $month
+    elif [ $MACHINE = "LOTUS" ]; then
+      echo "sbatch "+${MACHINE,,}+"_pre_process_en4_monthly.sh $start $month"
+      sbatch ${MACHINE,,}_pre_process_en4_monthly.sh $start $month
+    else
+      echo "not expecting $MACHINE"
     fi
   done
 done
