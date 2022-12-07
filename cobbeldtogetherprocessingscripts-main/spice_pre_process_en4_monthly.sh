532f68df7b2306ddc34ecab4de56444ff08713ee
jpolton <jpolton@gmail.com>
2022-12-07 20:37:07 +0000

    Add spice preprocessing launcher

--- cobbeldtogetherprocessingscripts-main/spice_pre_process_en4_monthly.sh
+++ cobbeldtogetherprocessingscripts-main/spice_pre_process_en4_monthly.sh
@@ -1,14 +1,11 @@
-#!/bin/bash 
-#SBATCH --partition=short-serial 
-#SBATCH -o %A_%a.out
-#SBATCH -e %A_%a.err
-#SBATCH --time=30:00
-#SBATCH --array=1-132%10
-
-## THIS FILES NEEDS EDITTING TO RUN ON SPICE AT THE MO
-
-module add jaspy
+#! /bin/ksh
+#SBATCH --mem=200000
+#SBATCH --ntasks=1
+#SBATCH --output=pre_en4_output
+#SBATCH --error=pre_en4_error
+#SBATCH --time=180
 source activate $CONDA_ENV
 
+#python pre_process_en4_1990_2020.py
 # Pass in integer year and month: python pre_process_en4_monthly.py <year> <month>
 python pre_process_en4_monthly.py $1 $2
