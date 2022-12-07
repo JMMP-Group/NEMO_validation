fd26aca06199ced9afdab5261c44eabb188e5140
jpolton <jpolton@gmail.com>
2022-12-07 21:37:14 +0000

    add wrapper for profile diff diagnostics

---
+++ cobbeldtogetherprocessingscripts-main/lotus_ana_MOD_METEST.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#SBATCH --partition=short-serial
+#SBATCH -o %A_%a.out
+#SBATCH -e %A_%a.err
+#SBATCH --time=30:00
+#SBATCH --array=1-132%10
+module add jaspy
+source activate $CONDA_ENV
+
+# SPICE
+# X##! /bin/ksh
+#X##SBATCH --mem=40000
+#X##SBATCH --ntasks=1
+#X##SBATCH --output=ana_output
+#X##SBATCH --error=ana_error
+#X##SBATCH --time=180
+
+#time -v python  GEN_MOD_Dave_example_profile_validation_SEAS.py $1 $2  $3 $4 > OUTPUTS/OUT_$1_$2_$3_$4
+#time -v python  GEN_MOD_Dave_example_profile_validation_METEST.py $1 $2  $3 $4  > OUTPUTS/OUT_$1_$2_$3_$4
+echo $1 $2 $3 $4 $4
+time -v python  GEN_MOD_Dave_example_profile_validation.py $1 $2 $3 $4 $5  > OUTPUTS/OUT_$1_$2_$3_$4_$5
---
+++ cobbeldtogetherprocessingscripts-main/spice_ana_MOD_METEST.sh
@@ -0,0 +1,11 @@
+#! /bin/ksh
+#SBATCH --mem=40000
+#SBATCH --ntasks=1
+#SBATCH --output=ana_output
+#SBATCH --error=ana_error
+#SBATCH --time=180
+
+#time -v python  GEN_MOD_Dave_example_profile_validation_SEAS.py $1 $2  $3 $4 > OUTPUTS/OUT_$1_$2_$3_$4
+#time -v python  GEN_MOD_Dave_example_profile_validation_METEST.py $1 $2  $3 $4  > OUTPUTS/OUT_$1_$2_$3_$4
+echo $1 $2 $3 $4 $4
+time -v python  GEN_MOD_Dave_example_profile_validation.py $1 $2 $3 $4 $5  > OUTPUTS/OUT_$1_$2_$3_$4_$5
