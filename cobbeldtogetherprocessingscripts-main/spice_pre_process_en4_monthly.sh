2a25b12fba65bfce15f31ef2f6b15adf896a9776
jpolton <jpolton@gmail.com>
2022-12-07 17:44:02 +0000

    add space pre processing file

---
+++ cobbeldtogetherprocessingscripts-main/spice_pre_process_en4_monthly.sh
@@ -0,0 +1,13 @@
+#!/bin/bash 
+#SBATCH --partition=short-serial 
+#SBATCH -o %A_%a.out
+#SBATCH -e %A_%a.err
+#SBATCH --time=30:00
+#SBATCH --array=1-132%10
+
+## THIS FILES NEEDS EDITTING TO RUN ON SPICE AT THE MO
+
+module add jaspy
+source ~/envs/coast/bin/activate
+# Pass in integer year and month: python pre_process_en4_monthly.py <year> <month>
+python pre_process_en4_monthly.py $1 $2
