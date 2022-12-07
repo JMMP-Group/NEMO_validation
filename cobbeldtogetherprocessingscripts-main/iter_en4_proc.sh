825a7b479c0d47ed0ce774284fb513b903552c89
jpolton <jpolton@gmail.com>
2022-12-07 17:46:53 +0000

    generalise preprocessing with global variables

--- cobbeldtogetherprocessingscripts-main/iter_en4_proc.sh
+++ cobbeldtogetherprocessingscripts-main/iter_en4_proc.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 echo "Bash version ${BASH_VERSION}..."
-for (( start=1980; start<1981; start++ ))
+source config.sh
+
+#for (( start=1980; start<1981; start++ ))
+for (( start=$STARTYEAR; start<$(expr $ENDYEAR + 1); start++ ))
 do
   for (( month=1; month<13; month++ ))
-  do 
+  do
      end=$(expr $start + 1 )
 #     sbatch spice_ana_SEAS_P0.0.sh $SEAS $start $end
-     #echo "sbatch spice_ana_MOD.sh $start $month"
-     #sbatch spice_pre_process_en4_monthly.sh $start $month
-     echo "sbatch lotus_pre_process_en4_monthly.sh $start $month"
-     sbatch lotus_pre_process_en4_monthly.sh $start $month
+     if [ $MACHINE = "SPICE" ]; then
+      echo "sbatch spice_pre_process_en4_monthly.sh $start $month"
+      sbatch spice_pre_process_en4_monthly.sh $start $month  # THIS FILE IS MISSING
+     elif [ $MACHINE = "LOTUS" ]; then
+      echo "sbatch lotus_pre_process_en4_monthly.sh $start $month"
+      sbatch lotus_pre_process_en4_monthly.sh $start $month
+    fi
   done
 done
