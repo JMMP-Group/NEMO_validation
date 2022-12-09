5b3766024be245b57bf70a6369c129804b834ac5
jeff polton <jelt@noc.ac.uk>
2022-12-09 17:46:50 +0000

    extend month range to year

--- EN4_processing/iter_sub_METEST.sh
+++ EN4_processing/iter_sub_METEST.sh
@@ -11,8 +11,8 @@ rm LOGS/OUT* LOGS/*.err LOGS/*.out
 for (( start=$STARTYEAR; start<$(expr $ENDYEAR + 1); start++ ))
 
   do
-  #for (( month=1; month<13; month++ ))
-  for (( month=1; month<2; month++ ))
+  for (( month=1; month<13; month++ ))
+  #for (( month=1; month<2; month++ ))
     do 
      end=$(expr $start + 1 )
 #     sbatch spice_ana_SEAS_P0.0.sh $SEAS $start $end
