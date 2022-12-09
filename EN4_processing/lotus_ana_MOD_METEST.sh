6bf0ff46f546e948deec16bf6503a468944cb8ea
jeff polton <jelt@noc.ac.uk>
2022-12-09 17:04:25 +0000

    tidy sbatch header options

--- EN4_processing/lotus_ana_MOD_METEST.sh
+++ EN4_processing/lotus_ana_MOD_METEST.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 #SBATCH --partition=short-serial
-######SBATCH --partition=high-mem
 #SBATCH --mem=40000
 #SBATCH -o LOGS/%A_%a.out
 #SBATCH -e LOGS/%A_%a.err
@@ -8,14 +7,6 @@
 #SBATCH --ntasks=1
 module add jaspy
 source activate $CONDA_ENV
-
-# SPICE
-# X##! /bin/ksh
-#X##SBATCH --mem=40000
-#X##SBATCH --ntasks=1
-#X##SBATCH --output=ana_output
-#X##SBATCH --error=ana_error
-#X##SBATCH --time=180
 
 #time -v python  GEN_MOD_Dave_example_profile_validation_SEAS.py $1 $2  $3 $4 > OUTPUTS/OUT_$1_$2_$3_$4
 #time -v python  GEN_MOD_Dave_example_profile_validation_METEST.py $1 $2  $3 $4  > OUTPUTS/OUT_$1_$2_$3_$4
