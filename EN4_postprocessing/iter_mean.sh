80f7d75ec790f4bdb3d2106d1ee37d2dc3761528
jpolton <jpolton@gmail.com>
2022-12-15 19:51:05 +0000

    restore iter over 12 months

--- EN4_postprocessing/iter_mean.sh
+++ EN4_postprocessing/iter_mean.sh
@@ -5,8 +5,8 @@ source config.sh
 mkdir -p LOGS
 rm LOGS/*err LOGS/*out LOGS/*log
 
-#for (( month=1; month<13; month++ ))
-for (( month=1; month<2; month++ ))
+for (( month=1; month<13; month++ ))
+#for (( month=1; month<2; month++ ))
 do 
      echo "sbatch "${MACHINE,,}"_mean.sbatch $MOD $month"
      sbatch ${MACHINE,,}_mean.sbatch $MOD $month
