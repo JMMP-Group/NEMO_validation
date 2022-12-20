66fe1684635bbafa203f25fd457c2f2d4b04673a
jpolton <jpolton@gmail.com>
2022-12-20 17:12:19 +0000

    Add merge by season

---
+++ EN4_postprocessing/merge_season.py
@@ -0,0 +1,43 @@
+import xarray as xr
+import sys
+from dask.diagnostics import ProgressBar
+from config import config
+from coast import general_utils
+
+config = config() # initialise variables in python
+
+def extract_season(ds, season=None):
+    # Extract season if needed
+    if season is not None:
+        season_array = general_utils.determine_season(ds.time)
+        s_ind = season_array == season
+        ds = ds.isel(id_dim=s_ind)
+    return ds
+
+args = sys.argv
+model = args[1]  # MOD. Already loaded into config.dn_out directory path
+season = str(args[2])  # season: 'DJF', 'MAM', 'JJA', SON'
+
+# Merge over all available years: "????" are 4-digit year labels
+ds_index = xr.open_mfdataset(config.dn_out +
+                             'interpolated_profiles_p0_*.nc',
+                             combine='nested', concat_dim="id_dim", parallel=True)
+ds_index = extract_season(ds_index, season)
+
+ds_diff = xr.open_mfdataset(config.dn_out +
+                            'profile_errors_p0_*.nc',
+                            combine='nested', concat_dim="id_dim", parallel=True)
+ds_diff = extract_season(ds_diff, season)
+
+
+
+with ProgressBar():
+  ds_index.to_netcdf(config.dn_out+"%03s_PRO_INDEX.nc"%(season))
+  ds_diff.to_netcdf(config.dn_out+"%03s_PRO_DIFF.nc"%(season))
+
+print(f'File written to {config.dn_out+"%03s_PRO_INDEX.nc"%(season)}')
+print(f'File written to {config.dn_out+"%03s_PRO_DIFF.nc"%(season)}')
+
+
+
+
