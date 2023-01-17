50bd478a8fb99ac82441d1298d64e869d1542a6c
jeff polton <jelt@noc.ac.uk>
2023-01-17 12:45:40 +0000

    update paths on JASMIN

--- EN4_processing/lotus_config.sh
+++ EN4_processing/lotus_config.sh
@@ -12,7 +12,13 @@ export MOD="P0.0"  # Model reference name
 export GRID="CO7_EXACT_CFG_FILE.nc"  # contains the grid information for NEMO
 # options?: "domain_cfg_MEs_01-003_opt_v1.nc" #  "GEG_SF12.nc"
 # options?: "GEG_SF12.nc" CO7_EXACT_CFG_FILE.nc
-
+if [[ $MOD = "P0.0" ]]
+then
+ export MASS_DIR="xu-cb676"
+else
+ export MASS_DIR=""
+ echo "Not ready for that experiment"
+fi
 
 # Use to activate conda environment in $MACHINE_pre_process_en4_monthly.sh
 export CONDA_ENV="/home/users/jelt/miniconda3/envs/coast_dev"
@@ -26,21 +32,19 @@ export FN_CFG_NEMO=$DN_CFG"example_nemo_grid_t.json"
 
 # location of raw EN4 data
 #export DIN_EN4="/home/users/jelt/EN4/"
-export DIN_EN4="/gws/nopw/j04/class_vol2/senemo/shared/EN4/"
+export DIN_EN4="/gws/nopw/j04/class_vol2/senemo/shared/EN4/downloads/EN.4.2.2.profiles/"
 
 # prefix for preprocessed EN4 data (chunked into files by region and time)
 export REGION="AMM15"
 # location of preprocessed EN4 data
-#export DOUT_EN4="/home/users/jelt/tmp/"
-#export DOUT_EN4="/home/users/jelt/"$REGION"/"
-export DOUT_EN4=$DIN_EN4$REGION"/"
+export DOUT_EN4="/gws/nopw/j04/class_vol2/senemo/shared/EN4/processed/"$REGION"/"
 
 # directory for NEMO domain_cfg.nc file
 export DN_DOM="/gws/nopw/j04/jmmp_collab/CO9_AMM15/inputs/domains/"
 # directory for NEMO data files
-#fn_dat = "/home/users/deazer/SAMPLE_DATA_COAST_WORKFLOW/%s/DAILY/%s0*T.nc"%(exper,startyear)
-#fn_dat = "/scratch/fred/COMPARE_VN36_VN_4.0_TIDE_SSH/%s/DAILY/%s%02d*T.nc*"%(exper,startyear,month)
-#export DN_DAT="/home/users/deazer/SAMPLE_DATA_COAST_WORKFLOW/"$MOD"/DAILY/"
-export DN_DAT="/gws/nopw/j04/jmmp/CO9_AMM15/outputs/p0/daily/"  # Dave 25h data
-#export DN_OUT="/home/users/jelt/tmp/" #"$REGION"/" 
-export DN_OUT="/home/users/jelt/"$REGION"/25h/"
+#export DN_DAT="/gws/nopw/j04/jmmp/CO9_AMM15/outputs/p0/daily/"  # Dave 25h data
+export DN_DAT="/gws/nopw/j04/jmmp/MASS/"$MASS_DIR"/daily/"  # P0.0 24h ave
+
+# directory for analysis output
+#export DN_OUT="/home/users/jelt/tmp/"$REGION"/" 
+export DN_OUT="/gws/nopw/j04/jmmp/CO9_AMM15_validation/"$MOD"/profiles/"
