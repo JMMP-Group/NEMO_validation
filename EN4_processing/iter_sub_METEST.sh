330c67ca51b3215b8b67ba3fbe6f3e0c439e0a2c
jeff polton <jelt@noc.ac.uk>
2023-01-19 22:01:53 +0000

    tidy shell script

--- EN4_processing/iter_sub_METEST.sh
+++ EN4_processing/iter_sub_METEST.sh
@@ -4,22 +4,15 @@ source config.sh
 
 rm LOGS/OUT* LOGS/*.err LOGS/*.out
 
-#MOD=$1
 #GRID="domain_cfg_MEs_01-003_opt_v1.nc" #  "GEG_SF12.nc"
 #GRID=$2 #   "GEG_SF12.nc" CO7_EXACT_CFG_FILE.nc
 #for (( start=2004; start<2015; start++ ))
 for (( start=$STARTYEAR; start<$(expr $ENDYEAR + 1); start++ ))
-
-  do
+do
   for (( month=1; month<13; month++ ))
-  #for (( month=1; month<2; month++ ))
-    do 
+  do 
      end=$(expr $start + 1 )
-#     sbatch spice_ana_SEAS_P0.0.sh $SEAS $start $end
-     #sbatch spice_ana_MOD_METEST.sh $MOD $start $end $GRID
-     #sbatch  spice_ana_MOD_METEST.sh $MOD $start $month $end $GRID
      echo "sbatch "${MACHINE,,}"_ana_MOD_METEST.sh $MOD $start $month $end $GRID"
      sbatch -J ${start}${month}${MOD} ${MACHINE,,}_ana_MOD_METEST.sh $MOD $start $month $end $GRID
-#     sh spice_ana_MOD_METEST.sh $MOD $start $end $GRID
- done
+  done
 done
