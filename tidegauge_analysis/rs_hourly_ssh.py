f7c9edbb243ed75fb5547fa7e6d93d2ff05d642d
jeff polton <jelt@noc.ac.uk>
2023-02-01 11:04:01 +0000

    WIP

--- tidegauge_analysis/rs_hourly_ssh.py
+++ tidegauge_analysis/rs_hourly_ssh.py
@@ -10,7 +10,7 @@ run_name='P0.0'
 mass = 'xu-cb676'
 
 #fn_nemo_data = "/gws/nopw/j04/jmmp_collab/CO9_AMM15/outputs/hourly/{0}/*.nc".format(run_name)
-fn_nemo_data = "/gws/nopw/j04/jmmp/MASS/{0}/shelftmb/20140101_shelftmb_grid_T.nc".format(mass)
+fn_nemo_data = "/gws/nopw/j04/jmmp/MASS/{0}/shelftmb/2014*_shelftmb_grid_T.nc".format(mass)
 fn_nemo_domain = "/gws/nopw/j04/jmmp_collab/CO9_AMM15/inputs/domains/CO7_EXACT_CFG_FILE.nc"
 fn_nemo_cfg = "/home/users/jelt/GitHub/COAsT/config/example_nemo_grid_t.json"
 
@@ -23,29 +23,36 @@ constit = ['M2','S2','N2','K1','O1','P1','M4']
 #analyse_ssh_hourly(fn_nemo_data, fn_nemo_domain, fn_nemo_cfg, fn_obs, fn_out, constit_to_save=constit, chunks = {'time_counter':50})
 
 dn_out = "/home/users/jelt/GitHub/NEMO_validation/tidegauge_analysis/FIGS"
+
+## Use validate_ssh_tg_hourly.py
 fn_extr_out = "/gws/nopw/j04/jmmp/CO9_AMM15_validation/{0}/tg_analysis/ssh_hourly_extract_{1}.nc".format(run_name.upper(), run_name)
-if(0):
+if(0):  # Takes 7mins for one month. 1 yr in 2.5 hrs
   print(f"Start extract_ssh")
   extract_ssh(fn_nemo_data, fn_nemo_domain, fn_nemo_cfg, fn_obs, fn_extr_out,
                      chunks = {'time_counter':100}, dist_omit = 5)
 
 fn_analyse_out = "/gws/nopw/j04/jmmp/CO9_AMM15_validation/{0}/tg_analysis/ssh_hourly_analyse_{1}.nc".format(run_name.upper(), run_name)
-if(0):
+
+if(1):
   print(f"Start analyse_ssh")
+  fn_extr_out = "/gws/nopw/j04/jmmp/CO9_AMM15_validation/P0.0/tg_analysis/ssh_hourly_extract_2014_P0.0.nc"
   analyse_ssh(fn_extr_out, fn_analyse_out, thresholds = np.arange(-.4, 2, 0.1),
                 constit_to_save = ['M2','S2','K2','N2','K1','O1','P1','Q1'],
                 semidiurnal_constit = ['M2','S2','K2','N2'],
                 diurnal_constit = ['K1','O1','P1','Q1'],
                 apply_ntr_filter = True )
-if(0):
+if(0):  # THIS PRODUCES CORR BUT GETS STUCK WITH HARMONICS PLOTS - NO DATA.
   fn_ssh_hourly_stats = fn_analyse_out
+  #fn_ssh_hourly_stats = "/gws/nopw/j04/jmmp/CO9_AMM15_validation/P0.0/tg_analysis/ssh_hourly_analyse_2014_P0.0.nc"
   plot_single_cfg( fn_ssh_hourly_stats, dn_out, run_name, file_type='.png')
 
+## Use analyse_ssh_hourly.py
 fn_analyse_out_b = "/gws/nopw/j04/jmmp/CO9_AMM15_validation/{0}/tg_analysis/ssh_hourly_analyse_b_{1}.nc".format(run_name.upper(), run_name)
-if(1):
+if(0):
   analyse_ssh_hourly( fn_nemo_data, fn_nemo_domain, fn_nemo_cfg, fn_obs, fn_analyse_out_b,
              thresholds=np.arange(0, 2, 0.1),
              constit_to_save=['M2', 'S2', 'K1', 'O1'],
              chunks={'time_counter': 100})
+if(0):  # THIS PRODUCES PLOTS WITH NaNs? FOR VALUES ON MAPS
   fn_ssh_hourly_stats = fn_analyse_out_b
   plot_stats_ssh_hourly_single_cfg( fn_ssh_hourly_stats, dn_out, run_name, file_type='.png')
