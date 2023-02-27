ee47036bb87072cdd2852d17310b27a59fb02695
jpolton <jpolton@gmail.com>
2023-02-27 20:33:18 +0000

    remove redundant config

--- harmonics_compare/iter_preprocess_harm.sh
+++ harmonics_compare/iter_preprocess_harm.sh
@@ -4,7 +4,7 @@ source config.sh  # sets $MACHINE. Is called again before other variables are us
 
 rm LOGS/OUT* LOGS/*.err LOGS/*.out
 
-configArray=("GS1p0_notide" "GS1p1_tide" "GS1p2_full" "FES2014")
+configArray=("GS1p1_tide" "GS1p2_full" "FES2014")
 for config in ${configArray[*]}
 do
   echo "sbatch "${MACHINE,,}"_pre_process_harm.sh $config"
