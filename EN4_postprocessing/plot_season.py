f3daff84896b660c8d39c2490e6e195411868fb6
rdPatmore <ryapat@noc.ac.uk>
2023-08-09 16:33:43 +0100

    minor formatting changes

--- EN4_postprocessing/plot_season.py
+++ EN4_postprocessing/plot_season.py
@@ -35,8 +35,8 @@ fn_list_JJA = [config.dn_out+"JJA_mask_means_daily.nc",
 ## Specify the specific regions, their labels and order of appearance to plot.
 ## Region indexing to match EN4_postprocessing mean_season.py
 region_ind = [ 1, 7, 3, 2, 9, 5, 4, 6, 8] # Region indices (in analysis) to plot
-region_names = ['N. North Sea','S. North Sea','Eng. Channel','Outer Shelf',
-                'Irish Sea', 'Kattegat', 'Nor. Trench', 'FSC', 'Off-shelf']
+region_names = ['N. North\nSea','S. North\nSea','Eng.\nChannel','Outer\nShelf',
+                'Irish\nSea', 'Kattegat', 'Nor.\nTrench', 'FSC', 'Off-shelf']
 
 plot_zero_line  = True      # Plot a black vertical line at x = 0
 # Plot the mean bathymetric depth. 
@@ -51,12 +51,12 @@ ref_depth = np.concatenate((np.arange(1,100,2), np.arange(100,300,5),
 # Subplot axes settings
 n_r = 2              # Number of subplot rows
 n_c = 9              # Number of subplot columns
-figsize = (9,7)      # Figure size
+figsize = (6.5,5)      # Figure size
 sharey = True        # Align y axes
 sharex = False       # Align x axes
-subplot_padding = .5 # Amount of vertical and horizontal padding between plots
-# Whole figure padding as % (left, top, right, bottom)
-fig_pad = (.075, .075, .15, .1)  
+subplot_padding = .4 # Amount of vertical and horizontal padding between plots
+# Whole figure padding as % (left, bottom, right, top)
+fig_pad = (.09, .10, .15, .18)  
 max_depth = 150      # Maximum plot depth
 
 # Legend settings
@@ -64,14 +64,14 @@ legend_str = ["CO9p2","CO7"]
 legend_index = 6     # Axis index to put legend (flattened index, start from 0).
                      # Good to place in an empty subplot
 legend_pos = 'upper right' # Position for legend, using matplitlib legend string
-legend_fontsize = 10
+legend_fontsize = 8
 
 # Labels and Titles
 xlabelpos = (figsize[0]/2, 0)    # (x,y) position of xlabel
 ylabel = "Depth (m)"             # Ylabel string
 ylabelpos = (figsize[1]/2, 0)    # (x,y) position of ylabel
 
-label_fontsize = 8               # Fontsize of all labels
+label_fontsize = 6               # Fontsize of all labels
 label_fontweight = "normal"      # Fontweight to use for labels and subtitles
 title_fontsize = 13              # Fontsize of title
 title_fontweight = "bold"        # Fontweight to use for title
@@ -88,12 +88,12 @@ n_ax = n_r*n_c
 
 print(f"Check region names specified are consistent with mask file")
 for i in range(n_reg):
-    print(f"Panel label:({region_names[i]}) matches data label:
-         ({ds_list[0].region_names.values[region_ind[i]]})")
+    print(f"""Panel label:({region_names[i]}) matches data label:
+         ({ds_list[0].region_names.values[region_ind[i]]})""")
 
 # Loop over variable to plot
 for var_str in ["Temperature", "Salinity"]:
-    if var_str == "Temperature": units = "(deg C)"
+    if var_str == "Temperature": units = "($^{\circ}$C)"
     if var_str == "Salinity": units = "(-)"
 
     #for analysis_str in ["MAE", "STD", "BIAS"]:
@@ -115,7 +115,7 @@ for var_str in ["Temperature", "Salinity"]:
         var_name = "profile_{0}_{1}".format(tmp_str, var_str.lower())
 
         # Filename for the output
-        fn_out = "FIGS/regional_{0}_{1}.svg".format(var_name, run_name)
+        fn_out = "FIGS/regional_{0}_{1}.pdf".format(var_name, run_name)
 
         # Create plot and flatten axis array
         f, axs = plt.subplots(n_r, n_c, figsize=figsize,
@@ -155,14 +155,14 @@ for var_str in ["Temperature", "Salinity"]:
 
                 # set x lims
                 if var_str == 'Salinity':
-                    axs[row,ii].set_xlim(-0.1, 0.5)
+                    axs[row,ii].set_xlim(-0.1, 3.5)
                 if var_str == 'Temperature':
-                    axs[row,ii].set_xlim(-0.1, 2.5)
+                    axs[row,ii].set_xlim(-0.1, 4.0)
 
                 # Plot fixed lines at 0 and mean depth
                 if plot_zero_line:
                     axs[row,ii].plot([0,0], [0, max_depth], c='k',
-                                        lw=1, ls='-')
+                                        lw=0.5, ls='-')
                 if plot_mean_depth:
                     axs[row,ii].plot([axs[row,ii].get_xlim()[0], 
                                       axs[row,ii].get_xlim()[1]], 
@@ -173,26 +173,28 @@ for var_str in ["Temperature", "Salinity"]:
                 # Invert y axis
                 axs[row,ii].invert_yaxis()
 
-  # Make legend
-  f.legend(p, legend_str, fontsize = legend_fontsize,  bbox_to_anchor=(1.0,0.5))
+        # Make legend
+        f.legend(p, legend_str, fontsize = legend_fontsize,
+                 bbox_to_anchor=(1.0,0.5))
 
-  # Set Figure title and label axes
-  f.suptitle(fig_title, fontsize=title_fontsize, fontweight=title_fontweight)
-  f.text(0.5, 0.01, var_str + " " + units, ha='center')
-  f.text(0.01, 0.5, 'depth (m)', va='center', rotation=90)
+        # Set Figure title and label axes
+        f.suptitle(fig_title, fontsize=title_fontsize,
+                   fontweight=title_fontweight)
+        f.text(0.5, 0.01, var_str + " " + units, ha='center')
+        f.text(0.01, 0.5, 'depth (m)', va='center', rotation=90)
 
-  # Set x and y labels
-  f.text(xlabelpos[0], xlabelpos[1], xlabel, 
-       va = 'center', rotation='horizontal', 
-       fontweight=label_fontweight, fontsize=label_fontsize)
+        # Set x and y labels
+        f.text(xlabelpos[0], xlabelpos[1], xlabel, 
+             va = 'center', rotation='horizontal', 
+             fontweight=label_fontweight, fontsize=label_fontsize)
 
-  # Set tight figure layout
-  f.tight_layout(w_pad = subplot_padding, h_pad= subplot_padding)
-  f.subplots_adjust(left   = (fig_pad[0]), 
-                    bottom = (fig_pad[1]),
-                    right  = (1-fig_pad[2]),
-                    top    = (1-fig_pad[3]))
+        # Set tight figure layout
+        f.tight_layout(w_pad = subplot_padding, h_pad= subplot_padding)
+        f.subplots_adjust(left   = (fig_pad[0]), 
+                          bottom = (fig_pad[1]),
+                          right  = (1-fig_pad[2]),
+                          top    = (1-fig_pad[3]))
 
-  # Save plot maybe
-  if save_plot: 
-      f.savefig(fn_out)
+        # Save plot maybe
+        if save_plot: 
+            f.savefig(fn_out)
