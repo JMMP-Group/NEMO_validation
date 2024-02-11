dbf7d6a84a399c01cb549a72f62bb9bd4c6abba8
rdPatmore <ryapat@noc.ac.uk>
2024-02-11 10:49:13 +0000

    initial refactoring

--- EN4_postprocessing/plot_regions.py
+++ EN4_postprocessing/plot_regions.py
@@ -1,101 +1,127 @@
-'''
-For plotting the analysis regions.
-Uses COAsT example data
-'''
-
-
+from PythonEnvCfg.config import config
+config = config() # initialise variables in python
 
 import xarray as xr
 import matplotlib.pyplot as plt
-import sys
-
-coast_repo = "/Users/jelt/GitHub/COAsT"
-sys.path.append(coast_repo)
 import coast
 
-
-def region_plot(mask: xr.Dataset):
-	"""
-    Plot a map of masks in the MaskMaker object
-    Add labels
+class mask_plotting(object):
+    """
+    Mask plotting routines
     """
 
-	n_mask = mask.dims["dim_mask"]
-	offset = 10  # nonzero offset to make scaled-boolean-masks [0, >offset]
-	for j in range(0, n_mask, 1):
-		tt = (j + offset) * mask["mask"].isel(dim_mask=j).squeeze()
-		ff = tt.where(tt > 0).plot(
-			x="longitude", y="latitude", levels=range(offset, n_mask + offset + 1, 1), cmap='tab10', add_colorbar=False
-		)
+    def __init__(self):
+        #%% File settings
+        fn_cfg_nemo = config.fn_cfg_nemo
+        fn_dom_nemo = config.dn_dom + config.grid_nc
 
-	cbar = plt.colorbar(ff)
-	cbar.ax.get_yaxis().set_ticks([])
-	for j in range(0, n_mask, 1):
-		cbar.ax.text(
-			1 + 0.5,
-			offset + (j + 0.5),
-			mask["region_names"].isel(dim_mask=j).values,
-			ha="left",
-			va="center",
-			color="k",
-		)
-	cbar.ax.get_yaxis().labelpad = 15
-	plt.title(None)
+    def region_plot(self, mask: xr.Dataset):
+        """
+        Plot a map of masks in the MaskMaker object
+        Add labels
+        """
+    
+        n_mask = mask.dims["dim_mask"]
+        offset = 10  # nonzero offset to make scaled-boolean-masks [0, >offset]
+        for j in range(0, n_mask, 1):
+       	    tt = (j + offset) * mask["mask"].isel(dim_mask=j).squeeze()
+       	    ff = tt.where(tt > 0).plot(x="longitude", y="latitude", 
+                                       levels=range(offset,
+                                       n_mask + offset + 1, 1), cmap='tab10',
+                                       add_colorbar=False)
 
-#%% File settings
-fn_dom_nemo = "/Users/jelt/GitHub/COAsT/example_files/coast_example_nemo_domain.nc"
-fn_cfg_nemo = "/Users/jelt/GitHub/COAsT/config/example_nemo_grid_t.json"
+        cbar = plt.colorbar(ff)
+        cbar.ax.get_yaxis().set_ticks([])
+        for j in range(0, n_mask, 1):
+            cbar.ax.text(1 + 0.5,
+                         offset + (j + 0.5),
+                         mask["region_names"].isel(dim_mask=j).values,
+                         ha="left",
+                         va="center",
+                         color="k",
+                         )
+        cbar.ax.get_yaxis().labelpad = 15
+        plt.title(None)
 
-print('Doing regional analysis..')
+    def open_mask(self):
+        """ open existing mask created by regional mean by season """
 
-# load nemo lat/lon grid to define regions (as function of bathymetry)
-nemo = coast.Gridded(fn_domain=fn_dom_nemo, config=fn_cfg_nemo)
+        path = config.dn_out + "mask_xr.nc"
+        self.regional_mask = xr.open_dataset(path, chunks=-1)
 
-bath = nemo.dataset.bathymetry.values.squeeze()
-lon = nemo.dataset.longitude.values.squeeze()
-lat = nemo.dataset.latitude.values.squeeze()
+    def get_model_bathymetry(self):
+        """ get nemo model bathymetry """
+        
+        # load nemo lat/lon grid to define regions (as function of bathymetry)
+        nemo = coast.Gridded(fn_domain=fn_dom_nemo, config=fn_cfg_nemo)
+        
+        # alias variables
+        self.bath = nemo.dataset.bathymetry.values.squeeze()
+        self.lon = nemo.dataset.longitude.values.squeeze()
+        self.lat = nemo.dataset.latitude.values.squeeze()
 
+    def create_regional_mask(self, overlap=False):
+        """
+        Create regional mask
 
-# Define Regional Masks
+        This is replicated in regional_mean_by_season.py and
+        merge_mean_crps.py and thus not needed here.
+        TODO: make this a common function and remove from here?
+        """
+        
+        # get model data
+        self.get_model_bathymetry(self)
+       
+        # Define Regional Masks
+        mm = coast.MaskMaker()
+        masks_list = []
 
-mm = coast.MaskMaker()
-masks_list = []
-# Add regional mask for whole domain
-#masks_list.append(np.ones(lon.shape))  # 0
-masks_list.append(mm.region_def_nws_north_north_sea(lon, lat, bath))  # 1
-masks_list.append(mm.region_def_nws_outer_shelf(lon, lat, bath))  # 2
-masks_list.append(mm.region_def_nws_english_channel(lon, lat, bath))  # 3
-#masks_list.append(mm.region_def_nws_norwegian_trench(lon, lat, bath))  # 4
-masks_list.append(mm.region_def_nws_kattegat(lon, lat, bath))  # 5
-#masks_list.append(mm.region_def_nws_fsc(lon, lat, bath))  # 6
-masks_list.append(mm.region_def_nws_south_north_sea(lon, lat, bath))  # 7
-masks_list.append(mm.region_def_nws_off_shelf(lon, lat, bath))  # 8
-masks_list.append(mm.region_def_nws_irish_sea(lon, lat, bath))  # 9
+        # Add regional masks
+        masks_list.append(mm.region_def_nws_north_north_sea(lon, lat, bath))
+        masks_list.append(mm.region_def_nws_outer_shelf(lon, lat, bath))
+        masks_list.append(mm.region_def_nws_english_channel(lon, lat, bath))
+        masks_list.append(mm.region_def_nws_norwegian_trench(lon, lat, bath))
+        masks_list.append(mm.region_def_nws_kattegat(lon, lat, bath))
+        masks_list.append(mm.region_def_nws_fsc(lon, lat, bath))
+        masks_list.append(mm.region_def_nws_south_north_sea(lon, lat, bath))
+        masks_list.append(mm.region_def_nws_off_shelf(lon, lat, bath))
+        masks_list.append(mm.region_def_nws_irish_sea(lon, lat, bath))
+        
+        masks_names = ["N North Sea", "Outer shelf","Eng channel",
+                       "Norwegian Trench", "Kattegat", "FSC",
+                       "S North Sea", "Off shelf", "Irish Sea" ]
+        print("Size of names is ", len(masks_names[:]))
+        
+        self.mask_xr = mm.make_mask_dataset(lon, lat, masks_list, masks_names)
 
-#masks_names = ['whole_domain', 'northern_north_sea', 'outer_shelf', 'eng_channel', 'nor_trench',
-#			   'kattegat', 'fsc', 'southern_north_sea', 'off_shelf', 'irish_sea']
-masks_names = ['N North Sea','Outer shelf','Eng channel','Kattegat',
-	     'S North Sea', 'Off shelf', 'Irish Sea' ]
-print("Size of names is ", len(masks_names[:]))
+    def plot_regional_mask(self):
+        """ plot regional mask """
 
-mask_xr = mm.make_mask_dataset(lon, lat, masks_list, masks_names)
+        # get mask
+        self.open_mask()
 
+        # get bathymetry - TODO: this information should be added to mask file
+        self.get_model_bathymetry()
 
-region_plot(mask_xr)
-plt.contourf(lon,lat,bath, levels=(0,10), colors="w")
-plt.contour(lon,lat,bath, levels=(0,10), colors="k")
+        # plot
+        region_plot(mask_xr.drop(["Norwegian Trench", "FSC"]))
+        plt.contourf(lon,lat,bath, levels=(0,10), colors="w")
+        plt.contour(lon,lat,bath, levels=(0,10), colors="k")
 
-## Append regions for contouring. These overlap shaded regions
-masks_list.append(mm.region_def_nws_norwegian_trench(lon, lat, bath))  # 5
-masks_list.append(mm.region_def_nws_fsc(lon, lat, bath))  # 6
-masks_names = [ 'N North Sea', 'Outer shelf', 'Eng Channel', 'Kattegat',
-			    'S North_Sea', 'Off shelf', 'Irish Sea', 'Nor Trench', 'FSC']
-mask_xr = mm.make_mask_dataset(lon, lat, masks_list, masks_names)
+        # add Faroe Shetland Channel
+        plt.contour(mask_xr.longitude, mask_xr.latitude, 
+                    mask_xr.mask.isel(dim_mask=7),colors='r')
+        plt.annotate("FSC", (-5.5, 60.75))
 
-plt.contour(mask_xr.longitude, mask_xr.latitude, mask_xr.mask.isel(dim_mask=7),colors='r')
-plt.annotate("FSC", (-5.5, 60.75))
-plt.contour(mask_xr.longitude, mask_xr.latitude, mask_xr.mask.isel(dim_mask=8),colors='r')
-plt.annotate("Nor. Trench", (4, 60.1), ha="center", va="center", rotation=-60)
+        # add Norwegian Trench
+        plt.contour(mask_xr.longitude, mask_xr.latitude,
+                    mask_xr.mask.isel(dim_mask=8),colors='r')
+        plt.annotate("Nor. Trench", (4, 60.1), ha="center", va="center",
+                      rotation=-60)
 
-plt.savefig("FIGS/maskmaker_quick_plot.png")
-plt.close("all")
+        # save
+        plt.savefig("FIGS/maskmaker_quick_plot.png")
+
+if __name__ == "__main__":
+    mp = mask_plotting()
+    mp.plot_regional_mask()
