0647d92361fca1c5b8bb952657d8755e0c12d368
rdPatmore <ryapat@noc.ac.uk>
2024-12-11 14:11:51 +0000

    init add bootstrapping

--- EN4_processing/get_bias_angle.py
+++ EN4_processing/get_bias_angle.py
@@ -1,3 +1,12 @@
+from PythonEnvCfg.config import config
+cfg = config() # initialise variables in python
+
+import coast
+import cartopy.crs as ccrs
+import cartopy.feature as cfeature
+import xarray as xr
+import numpy as np
+import cmocean
 
 class bias_angle(object):
     """
@@ -17,34 +26,39 @@ class bias_angle(object):
 
         self.var = var
 
-    def get_bias_climatology(self, model)
+    def get_bias_climatology(self):
+
         # TODO rename to "full depth" not "near full depth"
         fn="near_full_depth_EN4_bias_by_season_by_region.nc"
-
+        
         # get data
-        ds_path = self.fn_path + fn
-        da = xr.open_dataset(ds_path)["diff_" + var]
+        path = self.fn_path + fn
+        self.da_0 = xr.open_dataset(path)["diff_" + self.var]
 
-        ds_path = self.fn_comp_path + fn
-        da_comp = xr.open_dataset(ds_path)["diff_" + var]
+        path = self.fn_comp_path + fn
+        self.da_1 = xr.open_dataset(path)["diff_" + self.var]
 
-    def get_bias_angle_ds(self, var, depth_var=True):
+    def get_bias_angle_ds(self, depth_var=True):
 
-        for j, region in enumerate(da.region_names.values):
+        print (self.da_1)
+        multiindex = ["season","time","latitude","longitude"]
+        da_0 = self.da_0.set_index(id_dim=multiindex).drop_duplicates("id_dim")
+        da_1 = self.da_1.set_index(id_dim=multiindex).drop_duplicates("id_dim")
+        da_0, da_1 = xr.align(da_0, da_1)
+
+        angle = np.arctan(da_0/da_1)
+        angle.name = f"diff {var} angle"
+
+    def get_bias_angle_hist(self):
+        """
+        calculate 1-d histogram from bias angle data
+        """
+
+        for j, region in enumerate(self.da_1.region_names.values):
             print ("region: ",  region)
-            da_r = da.sel(region_names=region)
-            da_comp_r = da_comp.sel(region_names=region)
-
-            multiindex = ["season","time","latitude","longitude"]
-            da_r = da_r.set_index(id_dim=multiindex).drop_duplicates("id_dim")
-            da_comp_r = da_comp_r.set_index(id_dim=multiindex).drop_duplicates("id_dim")
-            da_r, da_comp_r = xr.align(da_r, da_comp_r)
-
+            da_r0 = self.da_0.sel(region_names=region)
+            da_r1 = self.da_1.sel(region_names=region)
             # get angle
-            angle = np.arctan(da_r/da_comp_r)
-
-            seasons = ["DJF","MAM","JJA","SON"]
-            colours = ["r","g","b","c"]
 
             for i, season in enumerate(seasons):
                 ax = axs[j,i]
@@ -64,38 +78,8 @@ class bias_angle(object):
 
                     s = ax.hist(angle_1d, bins=100,
                                 range=[-np.pi/2,np.pi/2])
-                    ax.axvline(np.pi/4, c="orange")
-                    ax.axvline(-np.pi/4, c="orange")
 
 
-                ax.set_title(f"{season} {region}")
-
-        for ax in axs[:,0]:
-            ax.set_ylabel("Freq.")
-
-        for ax in axs[:,1:].flatten():
-            ax.set_yticklabels([])
-
-        for ax in axs.flatten():
-            ax.set_xticks([-np.pi/2,-np.pi/4,0,np.pi/4,np.pi/2])
-
-        for ax in axs[:-1,:].flatten():
-            ax.set_xticklabels([])
-
-        for ax in axs[-1,:]:
-            ax.set_xticklabels(["$-\pi/2$",
-                                "$-\pi/4$",
-                                "0",
-                                "$\pi/4$",
-                                "$\pi/2$"])
-            ax.set_xlabel(f"{var} bias")
-
-        plt.savefig(f"full_depth_{var}_bias_angle.png", dpi=600)
-
-    def get_bias_angle_hist(self):
-        """
-        calculate 1-d histogram from bias angle data
-        """
 
     def get_bootstrapped_bias_angle_hist(self):
         """
@@ -104,4 +88,7 @@ class bias_angle(object):
 
 
 
-ps =  plot_pointwise_surface()
+if __name__ == "__main__":
+    ba = bias_angle("temperature")
+    ba.get_bias_climatology()
+    ba.get_bias_angle_ds()
