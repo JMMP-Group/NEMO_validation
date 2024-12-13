afd44c6a85e5ec6d2d3f2a3f4c04fd8b8f578ee0
rdPatmore <ryapat@noc.ac.uk>
2024-12-13 16:21:27 +0000

    separate plotting from processing

---
+++ EN4_postprocessing/plot_bias_angle.py
@@ -0,0 +1,82 @@
+from PythonEnvCfg.config import config
+cfg = config() # initialise variables in python
+
+import matplotlib.pyplot as plt
+import cartopy.crs as ccrs
+import xarray as xr
+import numpy as np
+import matplotlib
+
+matplotlib.rcParams.update({'font.size': 8})
+
+class bias_angle(object):
+    """
+    ploting routine for pointwise surface temperature and salinity maps
+    """
+
+    def __init__(self):
+
+        # paths
+        self.fn_dom = cfg.dn_dom + cfg.grid_nc
+        self.fn_path = cfg.dn_out + "surface_maps/"
+        self.fn_comp_path = cfg.comp_case["proc_data"] + "surface_maps/"
+        en4_nc = "/surface_maps/en4_gridded_surface_climatology.nc"
+        m_nc = "/surface_maps/binned_model_surface_climatology.nc"
+        self.en4_grid = cfg.dn_out + en4_nc
+        self.model_binned = cfg.dn_out + m_nc
+
+    def plot_bias_angle(self, var, depth_var=True):
+
+        # TODO rename to "full depth" not "near full depth"
+        fn=f"bootstrapped_{var}_bias_with_EN4.nc"
+
+        # get data
+        ds_path = cfg.dn_out + fn
+        da = xr.open_dataset(ds_path)
+
+        # initiate plots
+        fig, axs = plt.subplots(9,4, figsize=(6.5,8.5))
+        plt.subplots_adjust(hspace=0.4,bottom=0.05,top=0.95)
+        colours = ["r","g","b","c"]
+        for i, (region_name, region) in enumerate(da.groupby("region_names")):
+            for j, (season, subset) in enumerate(region.groupby("season")):
+                ax = axs[i,j]
+
+                s = ax.plot(subset.bias_angle,
+                            subset.temperature_frequency_quant.squeeze()[0])
+                s = ax.plot(subset.bias_angle,
+                            subset.temperature_frequency_quant.squeeze()[1])
+                s = ax.plot(subset.bias_angle,
+                            subset.temperature_frequency_quant.squeeze()[2])
+                s = ax.plot(subset.bias_angle,
+                           subset.temperature_frequency_mean.squeeze(),c='pink')
+                ax.axvline(np.pi/4, c="orange")
+                ax.axvline(-np.pi/4, c="orange")
+
+                ax.set_title(f"{season} {region_name}")
+
+        for ax in axs[:,0]:
+            ax.set_ylabel("Freq.")
+
+        for ax in axs[:,1:].flatten():
+            ax.set_yticklabels([])
+
+        for ax in axs.flatten():
+            ax.set_xticks([-np.pi/2,-np.pi/4,0,np.pi/4,np.pi/2])
+
+        for ax in axs[:-1,:].flatten():
+            ax.set_xticklabels([])
+
+        for ax in axs[-1,:]:
+            ax.set_xticklabels(["$-\pi/2$",
+                                "$-\pi/4$",
+                                "0",
+                                "$\pi/4$",
+                                "$\pi/2$"])
+            ax.set_xlabel(f"{var} bias")
+
+        png_name = f"FIGS/full_depth_{var}_bias_angle_bootstrapped.png"
+        plt.savefig(png_name, dpi=600)
+
+bs =  bias_angle()
+bs.plot_bias_angle("temperature")
--- EN4_processing/get_bias_angle.py
+++ EN4_processing/get_bias_angle.py
@@ -1,16 +1,14 @@
 from PythonEnvCfg.config import config
 cfg = config() # initialise variables in python
 
-import coast
-import cartopy.crs as ccrs
-import cartopy.feature as cfeature
 import xarray as xr
 import numpy as np
-import cmocean
+from dask.diagnostics import ProgressBar
 
 class bias_angle(object):
     """
-    ploting routine for pointwise surface temperature and salinity maps
+    method for pointwise surface temperature and salinity histograms comparing
+    bias across models referenced to EN4
     """
 
     def __init__(self, var):
@@ -29,6 +27,7 @@ class bias_angle(object):
     def get_bias_climatology(self):
 
         # TODO rename to "full depth" not "near full depth"
+        # TODO add season dimension
         fn="near_full_depth_EN4_bias_by_season_by_region.nc"
         
         # get data
@@ -40,55 +39,105 @@ class bias_angle(object):
 
     def get_bias_angle_ds(self, depth_var=True):
 
-        print (self.da_1)
+        # flatten depth to make 2d (region,id_dim) array
+        self.da_0 = self.flatten_across_depth(self.da_0)
+        self.da_1 = self.flatten_across_depth(self.da_1)
+
         multiindex = ["season","time","latitude","longitude"]
-        da_0 = self.da_0.set_index(id_dim=multiindex).drop_duplicates("id_dim")
-        da_1 = self.da_1.set_index(id_dim=multiindex).drop_duplicates("id_dim")
+        da_0 = self.da_0.set_index(id_dim=multiindex)#.drop_duplicates("id_dim")
+        da_1 = self.da_1.set_index(id_dim=multiindex)#.drop_duplicates("id_dim")
         da_0, da_1 = xr.align(da_0, da_1)
 
-        angle = np.arctan(da_0/da_1)
-        angle.name = f"diff {var} angle"
+        self.angle = np.arctan(da_0/da_1)
+        self.angle.name = f"diff {self.var} angle"
 
-    def get_bias_angle_hist(self):
+    def flatten_across_depth(self, da):
+        da = da.swap_dims({"z_dim":"depth"})
+        da_depth = []
+        for i, depth in enumerate(da.depth):
+            da_depth.append(da.sel(depth=depth))
+        da_1d = xr.concat(da_depth, dim="id_dim")
+
+        return da_1d
+
+    def get_bias_angle_hist(self, bootstrapped=True):
         """
         calculate 1-d histogram from bias angle data
         """
 
-        for j, region in enumerate(self.da_1.region_names.values):
-            print ("region: ",  region)
-            da_r0 = self.da_0.sel(region_names=region)
-            da_r1 = self.da_1.sel(region_names=region)
-            # get angle
+        hist_set = []
+        for _, region in self.angle.groupby("region_names"):
+            for season, subset in region.groupby("season"):
+                if bootstrapped:
+                    bootstrapped_hist = \
+                                  self.get_bootstrapped_bias_angle_hist(subset,
+                                                                        season)
+                    hist_set.append(bootstrapped_hist)
 
-            for i, season in enumerate(seasons):
-                ax = axs[j,i]
+        hist_set_ds = xr.merge(hist_set)
 
-                angle_season = angle.sel(season=season)
+        fn = cfg.dn_out + f"bootstrapped_{self.var}_bias_with_EN4.nc"
+        with ProgressBar():
+            hist_set_ds.to_netcdf(fn)
 
-                if depth_var:
-                    print (depth_var)
-                    angle_season = angle_season.swap_dims({"z_dim":"depth"})
-
-                    angle_season_depth = []
-                    for i, depth in enumerate(angle.depth):
-                        angle_season_depth.append(
-                                                angle_season.sel(depth=depth))
-                    angle_1d = xr.concat(angle_season_depth, dim="id_dim")
-
-
-                    s = ax.hist(angle_1d, bins=100,
-                                range=[-np.pi/2,np.pi/2])
-
-
-
-    def get_bootstrapped_bias_angle_hist(self):
+    def get_bootstrapped_bias_angle_hist(self, ds, season, sample_size=1000):
         """
         Single instance bootstrap sample the bias angle and create histogram
         """
 
+        bootstrap_set = []
+        for i in range(sample_size):
+            print (i)
+            # randomised sampling
+            obs_num = ds.sizes["id_dim"]
+            random_sample = np.random.randint(obs_num, size=obs_num)
+            ds_randomised = ds#ds.isel(id_dim=random_sample)
 
+            # get histogram
+            hist, bin_edges = np.histogram(ds_randomised, bins=100,
+                                           range=[-np.pi/2,np.pi/2])
+            bin_centres = (bin_edges[1:] + bin_edges[:-1]) / 2
+            hist = np.expand_dims(hist, axis=[1,2])
+  
+            # assign to dataset
+            bootstrapped_hist = xr.Dataset(
+                          {f"{self.var}_frequency":(
+                           ["bias_angle","region_names","season"],
+                           hist)},
+                          {"bias_angle":bin_centres,
+                           "bias_angle_bin_edges": bin_edges,
+                           "season":xr.DataArray([season], dims="season"),
+                           "region_names":ds.region_names})
+                           #["bias_angle","region_names","season","sample"],
+                           #"sample":i})
+            #bootstrapped_hist = bootstrapped_hist.expand_dims(dim={"sample":i})
+
+            # append dataset to list
+            bootstrap_set.append(bootstrapped_hist)
+
+        # join boostrap hists as single dataset
+        bootstrap_set_ds = xr.concat(bootstrap_set, dim="sample")
+
+        # alias var name
+        var_str = f"{self.var}_frequency"
+
+        # get mean
+        mean = bootstrap_set_ds.mean("sample")
+        print (mean.to_dataarray())
+        print (sdkf)
+        mean = mean.rename({var_str:var_str + "_mean"})
+
+        # get percentiles
+        quant = bootstrap_set_ds.quantile([0.05, 0.5, 0.95], "sample")
+        quant = quant.rename({var_str:var_str + "_quant"})
+        
+        # merge statistics
+        bootstrap_statistics = xr.merge([mean, quant])
+
+        return bootstrap_statistics
 
 if __name__ == "__main__":
     ba = bias_angle("temperature")
     ba.get_bias_climatology()
     ba.get_bias_angle_ds()
+    ba.get_bias_angle_hist()
