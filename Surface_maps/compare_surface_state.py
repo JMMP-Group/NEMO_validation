ad85292338ad3cc955bdd2e97188b01b3c63bc5d
rdPatmore <ryapat@noc.ac.uk>
2025-01-08 17:23:01 +0000

    get cmems functions added

--- Surface_maps/compare_surface_state.py
+++ Surface_maps/compare_surface_state.py
@@ -1,10 +1,11 @@
-from config import config
-
-config = config() # initialise variables in python
+from PythonEnvCfg.config import config, bounds
+cfg = config() # initialise variables in python
 
 import coast
 import xarray as xr
 import os
+import copernicusmarine
+import matplotlib.pyplot as plt
 
 class extract_surface(object):
     def __init__(self):
@@ -40,5 +41,64 @@ class extract_surface(object):
         clim_mean = clim.make_climatology(ds, "season",
               fn_out=self.fn_out + "surface_state_climatology_binned_model.nc")
 
-es = extract_surface()
-es.surface_state_climatology_binned_model()
+class satellite(object):
+    """
+    Class for validating against satellite data
+    """
+
+    def create_cmems_login(self):
+        """ create login config file """
+
+        copernicusmarine.login()
+
+    def get_cmems(self, var="ssh"):
+        """ download cmems data """
+
+        bdy = bounds("AMM15")
+
+        data_request = {
+           "longitude" : [bdy.lonbounds[0], bdy.lonbounds[1]],
+           "latitude" : [bdy.latbounds[0], bdy.latbounds[1]],
+           "time" : ["2004-01-01", "2014-01-01"],
+        }
+        
+        if var == "sst":
+           data_request["fn"] = "cmems_obs-sst_atl_phy_nrt_l3s_P1D-m",
+           data_request["variables"] = "sea_surface_temperature"
+
+        if var = "ssh":
+           data_request["fn"] = "cmems_obs-sl_eur_phy-ssh_my_allsat-l4-duacs-0.0625deg_P1D"
+           data_request["variables"] = "adt"
+
+        # Load xarray dataset
+        self.ds = copernicusmarine.open_dataset(
+            dataset_id = data_request["fn"],
+            minimum_longitude = data_request["longitude"][0],
+            maximum_longitude = data_request["longitude"][1],
+            minimum_latitude = data_request["latitude"][0],
+            maximum_latitude = data_request["latitude"][1],
+            start_datetime = data_request["time"][0],
+            end_datetime = data_request["time"][1],
+            variables = data_request["variables"]
+        )
+        
+
+    def monthly_mean(self):
+        """ average over month """
+
+        self.ds = self.ds.resample(time='1ME').mean()
+
+    def quick_plot(self):
+        """ quick plot """
+
+        snapshot = self.ds.isel(time=0).adt
+
+        plt.pcolor(snapshot)
+        plt.show()
+
+if __name__ == "__main__":
+
+    sat = satellite()
+    sat.get_cmems()
+    sat.monthly_mean()
+    sat.quick_plot()
