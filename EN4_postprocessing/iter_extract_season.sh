e03ebcc064780ee7da0e9dba9e4a8c8be6ed2624
rdPatmore <ryapat@noc.ac.uk>
2025-05-28 15:09:55 +0100

    new extract submision scripts

---
+++ EN4_postprocessing/iter_extract_season.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+echo "Bash version ${BASH_VERSION}..."
+cd ../PythonEnvCfg/
+source config.sh
+cd ../EN4_processing
+
+mkdir -p LOGS
+rm LOGS/*err LOGS/*out LOGS/*log
+
+for season in DJF MAM JJA SON;
+do
+   echo "sbatch -J "${MOD}${season} ${MACHINE,,}"_extract_season.sbatch $season"
+   sbatch -J ${MOD}${season} ${MACHINE,,}_extract_season.sbatch $season
+done
