2292d93450bbf1797dfcb64797f44d504c44f1e0
jeff polton <jpolton@users.noreply.github.com>
2025-06-10 15:03:35 +0100

    add submission script

---
+++ TG_obs_preprocessing/scripts/submit_pre_process_obs.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "Bash version ${BASH_VERSION}..."
+source config.sh  # sets $MACHINE. Is called again before other variables are used
+
+rm LOGS/OUT* LOGS/*.err LOGS/*.out
+
+echo "sbatch "${MACHINE,,}"_pre_process_obs.sh"
+sbatch -J "pre_proc" ${MACHINE,,}_pre_process_obs.sh
+
