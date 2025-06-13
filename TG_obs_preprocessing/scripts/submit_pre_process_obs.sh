a592aebebdf92668468eedb3a7c83e9e2d086c77
jeff polton <jpolton@users.noreply.github.com>
2025-06-13 20:09:13 +0100

    exclude bad host on lotus

--- TG_obs_preprocessing/scripts/submit_pre_process_obs.sh
+++ TG_obs_preprocessing/scripts/submit_pre_process_obs.sh
@@ -5,5 +5,5 @@ source config.sh  # sets $MACHINE. Is called again before other variables are us
 rm LOGS/OUT* LOGS/*.err LOGS/*.out
 
 echo "sbatch "${MACHINE,,}"_pre_process_obs.sh"
-sbatch -J "pre_proc" ${MACHINE,,}_pre_process_obs.sh
+sbatch -J "pre_proc" ${MACHINE,,}_pre_process_obs.sh --exclude=host1185
 
