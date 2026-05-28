2d5bd1b337fad95999cccd785203f8ff3e6d2ed2
rdPatmore <ryapat@noc.ac.uk>
2026-05-28 15:01:18 +0100

    path updates

--- PythonEnvCfg/lotus_config.sh
+++ PythonEnvCfg/lotus_config.sh
@@ -26,27 +26,28 @@ export FN_CFG_NEMO=$DN_CFG"example_nemo_grid_t.json"
 
 # location of raw EN4 data
 #export DIN_EN4="/home/users/jelt/EN4/"
-export DIN_EN4="/gws/nopw/j04/class_vol2/senemo/shared/EN4/downloads/EN.4.2.2.profiles/"
-export DN_ERA5="/gws/nopw/j04/jmmp/ERA5/"
-export DN_ERA_INTERIM="/gws/nopw/j04/jmmp/ERA_interim/"
+export DIN_EN4="/gws/ssde/j25b/class_vol2/senemo/shared/EN4/downloads/EN.4.2.2.profiles/"
+export DN_ERA5="/gws/ssde/j25b/jmmp/ERA5/"
+export DN_ERA_INTERIM="/gws/ssde/j25b/jmmp/ERA_interim/"
+export DN_SWOT="/gws/ssde/j25b/jmmp/CO9_AMM15_validation/SWOT/"
 
 # prefix for preprocessed EN4 data (chunked into files by region and time)
 export REGION="AMM15"
 # location of preprocessed EN4 data
-export DOUT_EN4="/gws/nopw/j04/class_vol2/senemo/shared/EN4/processed/"$REGION"/"
+export DOUT_EN4="/gws/ssde/j25b/class_vol2/senemo/shared/EN4/processed/"$REGION"/"
 
 # directory for NEMO domain_cfg.nc file
-export DN_DOM="/gws/nopw/j04/jmmp/jmmp_collab/AMM15/DOMAIN_CFG/"
-export DN_DAT="/gws/nopw/j04/jmmp/MASS/"$MASS_DIR"/25hourm/"  # MOD 24h ave
+export DN_DOM="/gws/ssde/j25b/jmmp/jmmp_collab/AMM15/DOMAIN_CFG/"
+export DN_DAT="/gws/ssde/j25b/jmmp/MASS/"$MASS_DIR"/25hourm/"  # MOD 24h ave
 export GRID="GEG_SF12.nc"
 
 # directory for NEMO data files
-#export DN_DAT="/gws/nopw/j04/jmmp/CO9_AMM15/outputs/p0/daily/"  # Dave 25h data
+#export DN_DAT="/gws/ssde/j25b/jmmp/CO9_AMM15/outputs/p0/daily/"  # Dave 25h data
 if [[ $COMP_MOD = "co7" ]]; then
-  export COMP_DAT="/gws/nopw/j04/jmmp/MASS/rosie_mi-ao113_2004/25hourm/"  # MOD 24h ave
+  export COMP_DAT="/gws/ssde/j25b/jmmp/MASS/rosie_mi-ao113_2004/25hourm/"  # MOD 24h ave
   export COMP_GRID="CO7_EXACT_CFG_FILE.nc"  # contains the grid information for NEMO
 elif [[ $COMP_MOD = "P2.0" ]]; then
-   export DN_DAT="/gws/nopw/j04/jmmp/MASS/"$MASS_DIR"/25hourm/"  # MOD 24h ave
+   export DN_DAT="/gws/ssde/j25b/jmmp/MASS/"$MASS_DIR"/25hourm/"  # MOD 24h ave
    export COMP_GRID="GEG_SF12.nc"
 else
    echo "WARNING: grid not set find matching grid and add to lotus_config.sh"
@@ -56,5 +57,7 @@ fi
 
 # directory for analysis output
 #export DN_OUT="/home/users/jelt/tmp/"$REGION"/" 
-export DN_OUT="/gws/nopw/j04/jmmp/CO9_AMM15_validation/"$MOD"/"
-export COMP_OUT="/gws/nopw/j04/jmmp/CO9_AMM15_validation/"$COMP_MOD"/"
+export DN_OUT="/gws/ssde/j25b/jmmp/CO9_AMM15_validation/"$MOD"/"
+export COMP_OUT="/gws/ssde/j25b/jmmp/CO9_AMM15_validation/"$COMP_MOD"/"
+
+
